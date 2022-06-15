a80ebc2b9dfc7f1a454204f648453dee3a0ea8fd
Joanna Panimasz <pani.joanna93@gmail.com>
2022-06-15 14:56:24 +0200

    const tasks added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 {
+
+    const tasks = [];
+    
     const removeTask = (taskIndex) => {
         tasks.splice(taskIndex, 1);
         render();
@@ -47,7 +50,7 @@
             <button class="tasks__button tasks__button--toggleDone js-toggleDone">
             ${task.done ? "✓" : ""}
             </button>
-            <span class="tasks__content${ task.done ? " tasks__content--done" : ""}">
+            <span class="tasks__content${ task.done ? " tasks__content--toggleDone" : ""}">
             ${task.content}
             </span>
             <button class="tasks__button tasks__button--remove js-remove">
