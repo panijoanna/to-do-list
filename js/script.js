53cb534ae75702333b46ff118a217ab4af4c79fb
Joanna Panimasz <pani.joanna93@gmail.com>
2022-06-15 14:32:21 +0200

    new consts added (bindToggleDoneEvents and render

--- js/script.js
+++ js/script.js
@@ -25,4 +25,43 @@
             });
         });
     };
+
+    const bindToggleDoneEvents = () => {
+        const toggleDoneButtons = document.querySelectorAll(".js-toggleDone");
+
+        toggleDoneButtons.forEach((toggleDoneButton, taskIndex) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(taskIndex);
+            });
+        });
+    };
+
+    const render = () => {
+        let taskListHTMLContent = "";
+
+        for (const task of tasks) {
+            tasksListHTMLContent += `
+            <li
+            class="tasks__item js-task"
+            >
+            <button class="tasks__button tasks__button--toggleDone js-toggleDone">
+            ${task.done ? "✓" : ""}
+            </button>
+            <span class="tasks__content${ task.done ? " tasks__content--done" : ""}">
+            ${task.content}
+            </span>
+            <button class="tasks__button tasks__button--remove js-remove">
+            🗑
+            </button>
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = tasksListHTMLContent;
+
+        bindRemoveEvents();
+        bindToggleDoneEvents();
+    };
+
+
 }
