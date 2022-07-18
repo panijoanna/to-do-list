558296ec923ca8e50983919a662b0625631bfaae
Joanna Panimasz <pani.joanna93@gmail.com>
2022-07-18 11:51:40 +0200

    beautify js

--- js/script.js
+++ js/script.js
@@ -43,9 +43,7 @@
 
         for (const task of tasks) {
             listHTMLContent += `
-            <li
-            class="tasks__item js-task"
-            >
+            <li class="tasks__item js-task">
             <button class="tasks__button tasks__button--toggleDone js-toggleDone">
             ${task.done ? "✓" : ""}
             </button>
